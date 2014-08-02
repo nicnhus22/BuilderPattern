1efe9adc999537b04cb8eaa1e4050978849b10f7
Nicolas Husser <nicolas.husser@gmail.com>
2014-08-01 22:51:21 -0400

    Adding Pair class

---
+++ src/com/strings/Pair.java
@@ -0,0 +1,20 @@
+package com.strings;
+
+public class Pair {
+
+	private String key;
+	private String value;
+	
+	public Pair(String key, String value){
+		this.key = key;
+		this.value = value;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
