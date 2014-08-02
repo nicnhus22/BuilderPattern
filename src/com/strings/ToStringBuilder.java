fb1b0a65ff348e0f676b8c5c06492077220b8ce7
Nicolas Husser <nicolas.husser@gmail.com>
2014-08-01 23:00:04 -0400

    Adding own to stirng builder

---
+++ src/com/strings/ToStringBuilder.java
@@ -0,0 +1,31 @@
+package com.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToStringBuilder {
+
+	private List<Pair> string = new ArrayList<Pair>();
+	private String finalString;
+	
+	public void append(String key, String value){
+		string.add(new Pair(key, value));
+	}
+	
+	public String toString(){
+		finalString = new String();
+		
+		for(Pair attribute : string){
+			finalString += "the "+attribute.getKey()+" is "+attribute.getValue()+" ";
+		}
+		
+		return finalString;
+	}
+	
+	public String stringBeautifuler(String originalString){
+		originalString = originalString.trim();
+		originalString = originalString.substring(0, 1).toUpperCase() + originalString.substring(1);
+		return originalString;
+	}
+	
+}
