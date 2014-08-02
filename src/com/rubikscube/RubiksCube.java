c78bf9a294bda48b59f3d28994bc547174d27c60
Nicolas Husser <nicolas.husser@gmail.com>
2014-08-01 23:05:49 -0400

    Adding RubiksCube classes

---
+++ src/com/rubikscube/RubiksCube.java
@@ -0,0 +1,80 @@
+package com.rubikscube;
+
+import com.strings.ToStringBuilder;
+
+public class RubiksCube {
+
+	private String name;
+	private String type;
+	private String size;
+	
+	public RubiksCube(Builder builder) {
+		this.name = builder.name;
+		this.size = builder.size;
+		this.type = builder.type;
+	}
+	
+	public static class Builder {
+		
+		private String name;
+		private String type;
+		private String size;
+		
+		public Builder(){
+			this.name = "default";
+			this.type = RubiksCubeHelper.Type.REGULAR;
+			this.size = RubiksCubeHelper.Size._3x3;
+		}
+		
+		public Builder withName(String name){
+			this.name = name;
+			return this;
+		}
+		
+		public Builder withSize(String size){
+			this.size = size.toString();
+			return this;
+		}
+		
+		public Builder withType(String type){
+			this.type = type.toString();
+			return this;
+		}
+		
+		public RubiksCube build(){
+			return new RubiksCube(this);
+		}
+	}
+	
+	@Override
+	public String toString() {
+	    return new ToStringBuilder().append("name", this.name)
+	    		.append("size", this.size)
+	    		.append("type", this.type)
+	    		.toString();
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+}
---
+++ src/com/rubikscube/RubiksCubeHelper.java
@@ -0,0 +1,20 @@
+package com.rubikscube;
+
+public class RubiksCubeHelper {
+
+	public static final class Size {
+		public static final String _2x2 = "2x2";
+		public static final String _3x3 = "3x3";
+		public static final String _4x4 = "4x4";
+		public static final String _5x5 = "5x5";
+		public static final String _6x6 = "6x6";
+		public static final String _7x7 = "7x7";
+	}
+	
+	public static final class Type {
+		public static final String REGULAR  = "regular";
+		public static final String MEGAMINX = "megaminx";
+		public static final String PYARMINX = "piraminx";
+	}
+	
+}
